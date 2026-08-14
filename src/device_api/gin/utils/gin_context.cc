6629e9a2d5070d965bc244aea6fd756f52c6a5f6
Shane Snyder <shsnyder@nvidia.com>
2026-08-14 13:55:24 -0500

    Add build/runtime safety checks for GIN version
    Signed-off-by: Shane Snyder <shsnyder@nvidia.com>

--- src/device_api/gin/utils/gin_context.cc
+++ src/device_api/gin/utils/gin_context.cc
@@ -1,11 +1,31 @@
 #include "gin_context.h"
 #include "common.h"
 
+/*
+ * The build only produces these benchmarks against headers new enough for the
+ * GIN requirements below, so this guards the remaining case the build system
+ * cannot see: a library older than those headers loaded at run time.
+ */
+static void check_nccl_version(void) {
+  int runtimeVersion = 0;
+  NCCLCHECK_FATAL(ncclGetVersion(&runtimeVersion));
+  if (runtimeVersion < NCCL_VERSION(2, 30, 7)) {
+    fprintf(stderr,
+            "Incompatible NCCL versions. nccl-tests was compiled with NCCL %d, but is "
+            "running with NCCL %d. The GIN device API is not compatible with versions "
+            "before 2.30.7.\n",
+            NCCL_VERSION_CODE, runtimeVersion);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+}
+
 size_t gin_max_buffer_bytes(void) {
   return 20ULL * (1ULL << 31);
 }
 
 void gin_devComm_create(ncclComm_t comm, const args_t* args, gin_context_t* ctx) {
+  check_nccl_version();
+
   ncclDevCommRequirements_t reqs = NCCL_DEV_COMM_REQUIREMENTS_INITIALIZER;
   reqs.ginContextCount = args->num_ctas;
   reqs.ginQueueDepth = args->queue_depth;
